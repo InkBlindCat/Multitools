527f22a6bb872909233e1ef9610c0bd8fb7c07b7
InkBlindCat <inkblindcat@gmail.com>
2026-01-23 13:01:04 +0300

    Updated the mod to work with both release and pre-release versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.inkblindcat"
-version = "1.2.0"
+version = "1.2.1"
 val javaVersion = 25
 
 val appData = System.getenv("APPDATA") ?: (System.getenv("HOME") + "/.var/app/com.hypixel.HytaleLauncher/data")
