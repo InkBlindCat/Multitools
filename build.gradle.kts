de7b3170b176040a4a45da9d727a25be99884db8
InkBlindCat <inkblindcat@gmail.com>
2026-01-28 04:14:15 +0300

    Reduced maximum durability Added Russian localization

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.inkblindcat"
-version = "1.2.2"
+version = "1.2.3"
 val javaVersion = 25
 
 val appData = System.getenv("APPDATA") ?: (System.getenv("HOME") + "/.var/app/com.hypixel.HytaleLauncher/data")
