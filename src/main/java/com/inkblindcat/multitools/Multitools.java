d55eedef4154ce3fda0d4a13a807f463083d8eb1
InkBlindCat <inkblindcat@gmail.com>
2026-05-27 20:42:30 +0300

    Updated to the new Hytale version (0.5.1)

--- src/main/java/com/inkblindcat/multitools/Multitools.java
+++ src/main/java/com/inkblindcat/multitools/Multitools.java
@@ -1,45 +1,24 @@
 package com.inkblindcat.multitools;
 
-//import com.hypixel.hytale.server.core.event.events.entity.LivingEntityInventoryChangeEvent;
-import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
-import com.hypixel.hytale.server.core.inventory.InventoryChangeEvent;
-import com.hypixel.hytale.server.core.modules.interaction.interaction.config.Interaction;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-import com.hypixel.hytale.server.core.util.Config;
-//import com.inkblindcat.multitools.event.ContainerMtConfigSystem;
-import com.inkblindcat.multitools.event.InventoryChangeMtConfigEvent;
-//import com.inkblindcat.multitools.event.LivingEntityInventoryChangeMtConfigEvent;
-import com.inkblindcat.multitools.event.PlayerReadyMtConfigEvent;
 
 import javax.annotation.Nonnull;
 
 public class Multitools extends JavaPlugin {
     private static Multitools INSTANCE;
 
-    private final Config<MultitoolsConfig> config;
-
     public Multitools(@Nonnull JavaPluginInit init) {
         super(init);
         INSTANCE = this;
-        this.config = this.withConfig("Multitools", MultitoolsConfig.CODEC);
     }
 
     @Override
     protected void setup() {
         super.setup();
-        this.config.save();
-
-        this.getEventRegistry().registerGlobal(InventoryChangeEvent.class, InventoryChangeMtConfigEvent::onInventoryChangeEvent);
-//        this.getEventRegistry().registerGlobal(LivingEntityInventoryChangeEvent.class, LivingEntityInventoryChangeMtConfigEvent::onLivingEntityInventoryChangeEvent);
-//        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, PlayerReadyMtConfigEvent::onPlayerReady);
-//        this.getEntityStoreRegistry().registerSystem(new ContainerMtConfigSystem());
     }
 
     public static Multitools getInstance() {
         return INSTANCE;
-    }
-    public Config<MultitoolsConfig> getConfig() {
-        return config;
     }
 }
